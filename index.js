1152efa9ac45e56283027e1fc34225f765908d47
Pouti <90718276+Pouti-Coding@users.noreply.github.com>
2022-08-16 02:22:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const MainClient = require("./nanospace");
 const client = new MainClient();
 
+const express = require("express")
+const web = express()
+
+web.listen(3000, () => {})
+web.get("/", (req, res) => {
+  res.send("Đang hoạt động!")
+})
+
 client.connect()
 
 module.exports = client; 
